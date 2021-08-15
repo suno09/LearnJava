9838ccd1274b7204172087abd2c838929829ed69
suno09 <shemsou9@gmail.com>
2021-08-15 15:43:32 +0100

    add hackerrank -updated-

--- HackerRank/src/main/java/dz/sundev/hackerrank/Dequeue.java
+++ HackerRank/src/main/java/dz/sundev/hackerrank/Dequeue.java
@@ -3,10 +3,11 @@ package dz.sundev.hackerrank;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Dequeue {
@@ -22,25 +23,21 @@ public class Dequeue {
 
         List<Integer> nums = null;
         String fileName = "input10.txt";
-        int n, m = 0;
+        int n = 0, m = 0;
 
         //read file into stream, try-with-resources
         try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
-            Object[] streamArr = stream.toArray(); 
+            Object[] streamArr = stream.toArray();
             String[] nm = ((String) streamArr[0]).split("\\s");
             n = Integer.parseInt(nm[0]);
             m = Integer.parseInt(nm[1]);
             nums = Arrays.stream(((String) streamArr[1])
-                    .split("\\s"))
-                    .mapToInt(Integer::parseInt)
-                    .boxed()
+                            .split("\\s"))
+                    .map(Integer::parseInt)
                     .collect(Collectors.toList());
-            System.out.println(nums.size());
-            System.out.println(nums.get(nums.size()-1));
         } catch (IOException e) {
             e.printStackTrace();
         }
-
 
 
 //        IntStream.range(0, n)
@@ -54,7 +51,7 @@ public class Dequeue {
 
         long max = deque.stream().distinct().count();
 
-        for (int i = m; i < nums.size(); i++) {
+        for (int i = m; i < n; i++) {
             System.out.printf("%d/%d\n", i, nums.size());
             int num = nums.get(i);
 //            System.out.printf("%s, %d", deque, num);
